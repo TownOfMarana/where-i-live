47f0a6d5aaa20f4bff3a407864d8f9cf7fbb558f
Logan Garland <lgarland@marana.com>
2016-09-02 09:01:23 -0700

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name whereILiveApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the whereILiveApp
+ */
+angular.module('whereILiveApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name whereILiveApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the whereILiveApp
+ */
+angular.module('whereILiveApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
